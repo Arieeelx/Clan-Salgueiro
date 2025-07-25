bad06461605bd9f98c33163308199ded9b548cf7
Arielss <arielm.sereno.a@gmail.com>
2025-07-25 11:06:14 -0400

    Nuevo proyecto Android

---
+++ app/src/main/java/com/example/clan_salgueiro/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.clan_salgueiro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Thread.sleep(2000)
+    }
+}
