2efb7c93a5b1e75f9e1e02ebf683ee0def3acc10
Devar <arielm.sereno.a@gmail.com>
2025-09-10 18:49:18 -0300

    Se agrega sonido al mitad del tiempo y siempre en vertical

--- app/src/main/java/com/example/clan_salgueiro/Emom/EmomActivity.kt
+++ app/src/main/java/com/example/clan_salgueiro/Emom/EmomActivity.kt
@@ -15,6 +15,8 @@ import com.example.clan_salgueiro.R
 
 class EmomActivity: AppCompatActivity() {
 
+
+    // Se declaran variables para el uso del cronómetro de la app
     private lateinit var tiempoTextViewRonda: TextView
     private lateinit var tiempoTextViewTiempo: TextView
     private lateinit var barraProgreso: ProgressBar
@@ -25,6 +27,8 @@ class EmomActivity: AppCompatActivity() {
     private var cuentaRegresiva: CountDownTimer? = null
     private var sonidoRonda: MediaPlayer? = null
     private var sonidoCompletado: MediaPlayer? = null
+
+    private var sonidoMitad: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,12 +44,15 @@ class EmomActivity: AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        //Se declara variables para la conexión intent de las rondas, minutos y segundos
+
         val rondas = intent.getIntExtra("EMOM_RONDAS", 1)
         val minutos = intent.getIntExtra("EMOM_MINUTOS", 0)
         val segundos = intent.getIntExtra("EMOM_SEGUNDOS", 0)
 
         sonidoRonda = MediaPlayer.create(this, R.raw.alarm1)
         sonidoCompletado = MediaPlayer.create(this, R.raw.victoria)
+        sonidoMitad = MediaPlayer.create(this, R.raw.mitad)
 
         rondasTotales = rondas
         milisPorRonda = ((minutos * 60) + segundos) * 1000L
@@ -80,10 +87,18 @@ class EmomActivity: AppCompatActivity() {
                 val progreso = (milisPorRonda - millisUntilFinished).toInt()
                 barraProgreso.progress = progreso
 
+                //Deteccion de tiempos
                 val segundosFaltantes = (millisUntilFinished / 1000) % 60
                 val minutosFaltantes = (millisUntilFinished / 1000) / 60
                 tiempoTextViewRonda.text = String.format("Ronda %d de %d", rondasRecorridas, rondasTotales)
                 tiempoTextViewTiempo.text = String.format("%02d:%02d", minutosFaltantes, segundosFaltantes)
+
+                // Se agrega deteccion de mitad del tiempo
+                val mitadTiempo = milisPorRonda / 2
+                if (millisUntilFinished in (mitadTiempo - 500)..(mitadTiempo + 500)) {
+                    sonidoMitad?.start()
+
+                }
             }
 
             override fun onFinish() {
@@ -105,6 +120,8 @@ class EmomActivity: AppCompatActivity() {
         cuentaRegresiva?.cancel()
         sonidoRonda?.release()
         sonidoRonda = null
+        sonidoMitad?.release()
+        sonidoMitad = null
     }
 
 }
